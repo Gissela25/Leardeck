6a920907964a6c9fd4fee77e5b4ea2cdcc6adc2a
Jony-07 <jony25lopezml@gmail.com>
2023-03-31 21:10:28 -0600

    Preparing the environment

---
+++ ML190271SL190836Desafio02DSM104/app/src/main/java/com/dsm104/ml190272sl190836/learndeck/TematicaActivity.kt
@@ -0,0 +1,11 @@
+package com.dsm104.ml190272sl190836.learndeck
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TematicaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tematica)
+    }
+}
---
+++ ML190271SL190836Desafio02DSM104/app/src/main/java/com/dsm104/ml190272sl190836/learndeck/adaptador/AdaptadorFicha.kt
@@ -0,0 +1,4 @@
+package com.dsm104.ml190272sl190836.learndeck.adaptador
+
+class AdaptadorFicha {
+}
